af24bc6df13a21425d9d45d7fe699f041bd0ece6
22041217-Wang Aiqin <NymphK@163.com>
2022-09-06 21:01:43 +0800

    Finished light water on NVBoard

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -7,13 +7,25 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop* top);
 
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst_n = 0;
+  while (n -- > 0) 
+	  single_cycle();
+  dut.rst_n = 1;
+}
+
 int main() {
   nvboard_bind_all_pins(&dut);
   nvboard_init();
-
+  reset(10);
   while(1) {
     nvboard_update();
-	dut.eval();
+	single_cycle();
   }
   nvboard_quit();
 }
