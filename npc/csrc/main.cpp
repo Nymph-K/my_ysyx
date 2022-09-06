bc32ac15a3e5d5622754248ccd72825201d5d60b
22041217-Wang Aiqin <NymphK@163.com>
2022-09-06 20:26:46 +0800

    Connected to the NVBoard, and finished switch.

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,37 +1,20 @@
-#include "Vtop.h"
-#include "verilated.h"
-#include "stdio.h"
+#include <nvboard.h>
+#include <Vtop.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include "verilated_vcd_c.h" //可选，如果要导出vcd则需要加上
-#include <unistd.h>
 
-int main(int argc, char** argv, char** env) {
-	VerilatedContext* contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    Vtop* top = new Vtop{contextp};
-    
-	VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
-	contextp->traceEverOn(true); //打开追踪功能
-	top->trace(tfp, 0); //
-	tfp->open("wave.vcd"); //设置输出的文件wave.vcd
-	while (!contextp->gotFinish()) 
-	{ 
-		int a = rand() & 1;
-		int b = rand() & 1;
-		top->a = a;
-		top->b = b;
-		top->eval();
-		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-		
-		tfp->dump(contextp->time()); //dump wave
-		contextp->timeInc(1); //推动仿真时间
-		
-		assert(top->f == a ^ b);
-		//usleep(1000*500);
-	}
-    delete top;
-    tfp->close();
-	delete contextp;
-    return 0;
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  while(1) {
+    nvboard_update();
+	dut.eval();
+  }
+  nvboard_quit();
 }
+
