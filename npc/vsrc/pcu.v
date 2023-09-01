42fa1a1ef19f8e4947749930a4234972a01fb78e
22041217-Wang Aiqin <NymphK@163.com>
2023-09-01 23:36:45 +0800

    Fixed some BUGs.

--- npc/vsrc/pcu.v
+++ npc/vsrc/pcu.v
@@ -35,31 +35,37 @@ module pcu (
     wire [31:0] npc = pc_b_j ? dnpc : pc + 4;
 
     reg  discard_inst;
+    reg  wait_valid;
 
     always @(posedge clk) begin
         if(rst) begin
             inst_r_ready <= 'b1;
             discard_inst <= 'b0;
+            wait_valid   <= 'b0;
         end else begin
-            if (discard_inst) begin // if discard inst
+            if (discard_inst & wait_valid) begin // if discard inst & wait valid
                 if(inst_r_valid) begin // wait pre-inst fetch over
                     inst_r_ready <= 'b1; // fetch inst
                     discard_inst <= 'b0;
+                    wait_valid   <= 'b0;
                 end
             end else begin // else not discard inst
                 if(inst_r_ready) begin // if fetching inst
-                    if(pc_b_j) begin // if branch or jump
-                        if(if_busy) begin // if ifu busy not wait valid
-                            ;
-                        end else begin // if idle wait valid
+                    if(pc_b_j) begin // if branch or jump, discard inst
+                        if(wait_valid | ~if_busy) begin // if wait valid
                             inst_r_ready <= 'b0;
                             discard_inst <= 'b1;
                         end
-                    end else if(inst_r_valid) begin
-                        inst_r_ready <= pc_wen;
+                        wait_valid   <= wait_valid | ~if_busy; // if idle, wait valid
+                    end else if(inst_r_valid) begin // fetch over
+                        inst_r_ready <= pc_wen; // if pc update, fetch inst
+                        discard_inst <= 'b0;
+                        wait_valid   <= 'b0;
+                    end else begin
+                        wait_valid   <= wait_valid | ~if_busy; // if idle, wait valid
                     end
                 end else begin // else not fetching inst
-                    inst_r_ready <= pc_wen;
+                    inst_r_ready <= pc_wen;// if pc update, fetch inst
                 end
             end
 
